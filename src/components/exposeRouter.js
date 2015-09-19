15213ff1b5876aa356339b1adefd1c974585b7c6
Ruben Oostinga <roostinga@xebia.com>
2015-09-19 20:34:20 +0200

    Fix proptype router not being defined

--- src/components/exposeRouter.js
+++ src/components/exposeRouter.js
@@ -2,16 +2,16 @@ import React, {Component} from 'react';
 
 // Higher order component exposing router.
 // https://medium.com/@dan_abramov/mixins-are-dead-long-live-higher-order-components-94a0d2f9e750
-export default function exposeRouter(Component) {
+export default function exposeRouter(Wrapped) {
 
   class ExposeRouter extends Component {
     render() {
-      return <Component {...this.props} router={this.context.router} />;
+      return <Wrapped {...this.props} router={this.context.router}/>;
     }
   }
 
   ExposeRouter.contextTypes = {
-    router: React.PropTypes.object.isRequired
+    router: React.PropTypes.object
   };
 
   return ExposeRouter;

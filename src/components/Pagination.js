f8d3123fc4c289548867aeec33c1ee3ba81493a8
Ruben Oostinga <roostinga@xebia.com>
2015-09-27 19:35:03 +0200

    Add some stateless components

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,44 +1,41 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Link from 'react-router/lib/Link';
 
 const pagesAround = 5;
 const steps = 10;
 const productPerPage = 48;
 
-class PaginationLinks extends Component {
-  static getPages(page, total) {
-    const pages = [];
-    for (let i = 1; i <= total; i++) {
-      if (
-        i === 1 ||
-        i % steps === 0 ||
-        page - pagesAround <= i && i <= page + pagesAround ||
-        i === total) {
-        pages.push(i);
-      }
+function getPages(page, total) {
+  const pages = [];
+  for (let i = 1; i <= total; i++) {
+    if (
+      i === 1 ||
+      i % steps === 0 ||
+      page - pagesAround <= i && i <= page + pagesAround ||
+      i === total) {
+      pages.push(i);
     }
-    return pages;
   }
-
-  render() {
-    const { page: currentPage, total, query} = this.props;
-    const totalPages = Math.ceil(total / productPerPage);
-    return (
-      <div style={{textAlign: 'center'}}>
-        <ul className="pagination">{PaginationLinks.getPages(currentPage, totalPages).map((page) =>
-            <li key={page} className={page === currentPage ? 'active' : null}>
-              <Link to="/products" query={{...query, page}}>{page}</Link>
-            </li>
-        )}</ul>
-      </div>
-    );
-  }
+  return pages;
 }
 
-export default PaginationLinks;
+const PaginationLinks = ({ page: currentPage, total, query}) => {
+  const totalPages = Math.ceil(total / productPerPage);
+  return (
+    <div style={{textAlign: 'center'}}>
+      <ul className="pagination">{getPages(currentPage, totalPages).map((page) =>
+          <li key={page} className={page === currentPage ? 'active' : null}>
+            <Link to="/products" query={{q: query, page}}>{page}</Link>
+          </li>
+      )}</ul>
+    </div>
+  );
+};
 
 PaginationLinks.propTypes = {
   total: React.PropTypes.number.isRequired,
   page: React.PropTypes.number.isRequired,
   query: React.PropTypes.string.isRequired
 };
+
+export default PaginationLinks;
--- src/components/ProductImage.js
+++ src/components/ProductImage.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
-export default class ProductImage extends React.Component {
-  render() {
-    const productNumber = this.props.productNumber;
-    const normalizedName = this.props.normalizedName;
-    const src = 'https://assets.wehkamp.com/i/wehkamp/' + productNumber + '_pb_01/' + normalizedName + '.jpg?$product300x300$';
-    return (
-      <img className="img-responsive" src={src} alt={this.props.alt}/>
-    );
-  }
-}
+const ProductImage = ({productNumber, normalizedName, alt}) =>{
+  const src = 'https://assets.wehkamp.com/i/wehkamp/' + productNumber + '_pb_01/' + normalizedName + '.jpg?$product300x300$';
+  return (
+    <img className="img-responsive" {...{src, alt}}/>
+  );
+};
+
 
 ProductImage.propTypes = {
   productNumber: React.PropTypes.string.isRequired,
   normalizedName: React.PropTypes.string.isRequired,
   alt: React.PropTypes.string.isRequired
 };
+
+export default ProductImage;
